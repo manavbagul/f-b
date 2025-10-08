316dc722c365c5c7af06982263575796877b32da
Manav <manavbagul.profile@gmail.com>
2025-10-08 11:28:47 -0700

    hello

--- api/test.js
+++ api/test.js
@@ -1,8 +1,3 @@
 export default async function handler(req, res) {
-  if (req.method !== "GET") {
-    res.status(405).json({ error: "Method not allowed" });
-    return;
-  }
-
   res.status(500).json({ message: "server working" });
 }
