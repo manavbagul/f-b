993d7cdb81abc2862e1bbf669374b6a9a264079a
Manav <manavbagul.profile@gmail.com>
2025-10-08 21:07:14 -0700

    tt

--- api/application.js
+++ api/application.js
@@ -1,10 +1,10 @@
 import { pdf_build } from "../lib/pdf-builder-file";
 
 export default async function handler(req, res) {
-  // if (req.method !== "POST") {
-  //   res.status(405).json({ error: "Method not allowed" });
-  //   return;
-  // }
+  if (req.method !== "POST") {
+    res.status(405).json({ error: "Method not allowed" });
+    return;
+  }
 
   try {
     const fields = req.body;
